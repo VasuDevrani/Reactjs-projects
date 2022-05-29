7fe2bf215db203c318da2babcc06b75fe276a1fe
VasuDevrani <vasu2002devrani@gmail.com>
2022-05-29 19:40:25 +0530

    Navbar files

---
+++ Navbar/navbar/src/component/Navbar/navbar.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react';
+import { Menuitems } from './Menuitems.js';
+import './navbar.css'
+// import {Button} from '../Button'
+
+export default class Navbar extends Component{
+
+    state = { clicked: false}
+
+    handleClick =()=>{
+        this.setState({clicked: !this.state.clicked})
+    }
+
+    render(){
+        return (
+            <nav className='navbarItem'>
+                <h1 className="navbar-logo">React 🙂</h1>
+                <div className="menu-icon" onClick = {this.handleClick}>
+                    <ion-icon name={this.state.clicked ? "contract-outline": "filter-outline"}></ion-icon>
+                </div>
+                <ul className={this.state.clicked ? 'nav-menu active' : 'nav-menu'}>
+                    {/* new form of map function */}
+                    { Menuitems.map((item,ind) => {
+                        return(
+                        <li key={ind}><a className={item.cName} href={item.url} >
+                            {item.title}
+                        </a></li>
+                        )
+                    })}
+                </ul>
+                {/* <Button>Sign Up</Button> */}
+            </nav>
+        )
+    }
+}
